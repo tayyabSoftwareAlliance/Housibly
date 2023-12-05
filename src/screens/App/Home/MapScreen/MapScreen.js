8349c2f2af1d03905c2bdc0c36be155ac418c18c
tayyabSoftwareAlliance <tayaab@softwarealliance.io>
2023-12-05 18:09:41 +0500

    changes

--- src/screens/App/Home/MapScreen/MapScreen.js
+++ src/screens/App/Home/MapScreen/MapScreen.js
@@ -1,8 +1,8 @@
-import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import { SafeAreaView, View } from 'react-native';
 import React from 'react';
-import {BackHeader, MapComponent} from '../../../../components';
+import { BackHeader, MapComponent } from '../../../../components';
 import styles from './styles';
-import {colors} from '../../../../shared/exporter';
+import { colors } from '../../../../shared/exporter';
 
 const MapScreen = () => {
   return (
--- src/screens/App/Home/MapScreen/styles.js
+++ src/screens/App/Home/MapScreen/styles.js
@@ -1,21 +1,17 @@
 import {StyleSheet} from 'react-native';
-import {WP, colors} from '../../../../shared/exporter';
+import {WP, colors,spacing} from '../../../../shared/exporter';
 
 const styles = StyleSheet.create({
   rootContainer: {
     flex: 1,
     backgroundColor: colors.over1,
   },
-  contentContainer: {
-    flex: 1,
-  },
   headerStyle: {
     position: 'absolute',
-    height: '11%',
-    zIndex: 9999,
+    zIndex: 1,
     justifyContent: 'flex-end',
     backgroundColor: colors.over1,
-    paddingBottom: 10,
+    ...spacing.py4
   },
 });
 
